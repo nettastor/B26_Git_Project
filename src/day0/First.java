ae738a1a3814a6ae51c14162da0017c08f5c9a58
netta <nettastor@gmail.com>
2022-03-26 16:10:32 -0400

    Added First Class

--- src/day0/First.java
+++ src/day0/First.java
@@ -1,7 +1,9 @@
 package day0;
 public class First {
-    //TODO: Works on this tomorrow
-    // add some cool code
+    public static void main(String[] args) {
+        System.out.println("java");
+        System.out.println("Working ...");
+    }
 
     // extra line 1
     // extra line 2
