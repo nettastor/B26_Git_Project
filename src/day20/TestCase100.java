b6fa045af8e10908ce5a83d6a7757ba9c5a276b3
netta <nettastor@gmail.com>
2022-03-29 21:57:50 -0400

    Fixed the spelling

--- src/day20/TestCase100.java
+++ src/day20/TestCase100.java
@@ -2,6 +2,6 @@ package day20;
 public class TestCase100 {
     //Testing for checkout
     //added 2 items to the cart
-    // Verufy ht total [price of the items
+    // Verify ht total [price of the items
     //verify the secure payment
 }
